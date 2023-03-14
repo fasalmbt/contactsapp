1fdfe7c10250998c5b8387ef9783a44c8db3e17c
fasalmbt <>
2023-03-14 14:38:05 +0530

    UI revamped

--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,7 @@ const contactController = require('../controllers/contact');
 const collection = require('../config/collection')
 var db = require('../config/connect')
 
-const verify = (req, res, next) => {
+const isAuthenticatedUser = (req, res, next) => {
     if (req.session.user) {
         next();
     } else {
@@ -13,12 +13,12 @@ const verify = (req, res, next) => {
     }
 }
 
-router.get('/', verify, (req, res) => {
+router.get('/', isAuthenticatedUser, (req, res) => {
     let user = req.session.user;
     res.render('user/dashboard', { user });
 });
 
-router.get('/dashboard', verify, (req, res) => {
+router.get('/dashboard', isAuthenticatedUser, (req, res) => {
     let user = req.session.user;
     res.render('user/dashboard', { user });
 });
@@ -31,22 +31,22 @@ router.get('/register', (req, res) => {
     res.render('user/signup');
 });
 
-router.get('/change-credentials/:id', verify, (req, res) => {
+router.get('/change-credentials/:id', isAuthenticatedUser, (req, res) => {
     let user = req.session.user;
     res.render('user/change-credentials', { user });
 });
 
-router.get('/add-contact', verify, (req, res) => {
+router.get('/add-contact', isAuthenticatedUser, (req, res) => {
     res.render('contact/add-contact');
 });
 
-router.get('/view-contact', verify, (req, res) => {
+router.get('/view-contact', isAuthenticatedUser, (req, res) => {
     contactController.viewContact().then((contacts) => {
         res.render('contact/view-contact', { contacts })
     })
 });
 
-router.get('/update-contact/:id', verify, (req, res) => {
+router.get('/update-contact/:id', isAuthenticatedUser, (req, res) => {
     contactController.getContact(req.params.id).then((contact) => {
         res.render('contact/update-contact', { contact });
     })
@@ -73,7 +73,7 @@ router.post('/register', (req, res) => {
     })
 });
 
-router.post('/add-contact', verify, (req, res) => {
+router.post('/add-contact', isAuthenticatedUser, (req, res) => {
     contactController.addContact(req.body).then((response) => {
         console.log(response);
         res.redirect('/user/dashboard')
@@ -94,13 +94,13 @@ router.post('/login', (req, res) => {
     })
 });
 
-router.post('/update-contact/:id', verify, (req, res) => {
+router.post('/update-contact/:id', isAuthenticatedUser, (req, res) => {
     contactController.updateContact(req.params.id, req.body).then(() => {
         res.redirect('/user/dashboard');
     })
 });
 
-router.post('/change-credentials/:id', verify, (req, res) => {
+router.post('/change-credentials/:id', isAuthenticatedUser, (req, res) => {
     userController.updateCredentials(req.params.id, req.body).then(() => {
         res.redirect('/user/dashboard');
     })
